8532661e1e1591ed9409437c7ca908b9e24883f9
thebotcandoit <matt@thebotcandoit.com>
2026-03-26 13:59:56 -0700

    Add Google Analytics (GA4) tracking to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
+import Script from 'next/script'
 import '../styles/globals.css'
 
+const GA_ID = 'G-VH8LTHVN9Z'
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="ga4-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
